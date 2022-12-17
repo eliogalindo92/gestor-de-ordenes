e4fc83f9fbd81f064c8d9e9999171dc747fdd662
= <=>
2022-12-16 22:55:22 -0500

    Actualizados los reportes, aún sin estilos.

--- resources/views/plan/exportar_plan.blade.php
+++ resources/views/plan/exportar_plan.blade.php
@@ -5,24 +5,24 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-{{--    <link rel="stylesheet" href="{{asset('css/themes/bootstrap.css')}}" type="text/css">--}}
+    <link rel="stylesheet" href="{{public_path('css/bootstrap.css')}}" type="text/css">
     <title>Plan de distribución en PDF</title>
 </head>
 <body>
-<div>
+<div class="container-fluid">
     <h3 align="center">Plan de distribución N°:  {{$plan->numero_plan}}</h3>
     <div class="row-content">
         <label><strong>Fecha de elaboración:</strong> <i>{{$plan->created_at}}</i></label>
     </div>
-    <div>
+    <div class="row-content">
         @foreach($entidades as $entidad)
             @if($plan->id_entidad == $entidad->id)
                 <label><strong>Realizada para:</strong> <i>{{$entidad->nombre_entidad}}</i></label>
             @endif
         @endforeach
     </div>
-    <div>
-        <table>
+    <div class="row-content">
+        <table class="table table-stripped table-hover">
             <thead>
             <th>
                 Fecha de distribución
